43758dd2ebb61f2feb990c8e0bf8185b499f220a
Is4ias <isaias02dev@gmail.com>
2025-11-11 14:57:46 -0300

    feat: usando UserCreationForm para criar novos usuários no django

--- contact/forms.py
+++ contact/forms.py
@@ -1,45 +1,52 @@
 from django.core.exceptions import ValidationError
 from django import forms
 from . import models
+from django.contrib.auth.forms import UserCreationForm
 
-
-class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+class ContactForm(forms.ModelForm): 
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class': 'classe-a class-b',
-                'placeholder': 'aqui veio do init',
+                'accept': 'image/*',
             }
-        ),
-        label='primeiro nome',
-        help_text='texto de ajuda para usuario',
+        )
     )
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a class-b',
-        #     'placeholder': 'aqui veio do init',
-        # })
-    
-    
-    
     class Meta:
         model = models.Contact
-        fields = [
-            'first_name', 'last_name', 'phone'
-            ]
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+            'picture',
+            )
     
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
         
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid',
-            )
-        )
-        
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome nao pode ser igual ao segundo',
+                code='invalid'
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+            
         return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+      
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'veio do add_error',
+                    code='invelid'
+                )
+            )
+        return first_name
+    
+    
+class RegisterForm(UserCreationForm):
+    ...
--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -3,3 +3,5 @@
 from .contact_forms import *
 
 from .contact_views import *
+
+from .user_forms import *
